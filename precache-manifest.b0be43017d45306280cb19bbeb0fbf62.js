b01e1aea959da2fbfaad9d5f076874b897596d9f
shirsath.chetan <shirsath.c@husky.neu.edu>
2020-06-22 21:43:57 -0400

    Updates

---
+++ precache-manifest.b0be43017d45306280cb19bbeb0fbf62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367e81c897fbcfa4dbb841ee389f2f4f",
+    "url": "/CovidApp/index.html"
+  },
+  {
+    "revision": "2140544227b07083d658",
+    "url": "/CovidApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a91a2f0bd1b5ebf0bf1",
+    "url": "/CovidApp/static/js/2.9840c9ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CovidApp/static/js/2.9840c9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2140544227b07083d658",
+    "url": "/CovidApp/static/js/main.75ce8bc7.chunk.js"
+  },
+  {
+    "revision": "ab446bbc5d0cda6ae1c2",
+    "url": "/CovidApp/static/js/runtime-main.7b25ef9f.js"
+  }
+]);
